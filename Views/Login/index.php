f2cfa7768499bdb5b807372962f23a0cc2f7ef92
VuNguyen26 <150309138+VuNguyen26@users.noreply.github.com>
2025-02-28 10:19:17 +0700

    update login

--- Views/Login/index.php
+++ Views/Login/index.php
@@ -6,25 +6,25 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<title>Valcloshop - Đăng Nhập</title>
 	<link
-      rel="icon"
-      type="image/x-icon"
-      href="./Views/images/avt.png"
+    rel="icon"
+    type="image/x-icon"
+    href="./Views/images/avatar.png"
     />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="./Views/Login/style.css">
 	<link href="./Views/Navbar/navbar.css" rel="stylesheet" type="text/css" />
 	
 	<link
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-      rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    rel="stylesheet"
     />
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,500;0,600;0,700;0,800;1,200;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet"
     />
     <script src="https://use.fontawesome.com/721412f694.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -39,7 +39,7 @@
 			<div class="card-wrapper col-md-4">
 				<div class="card fat p-1 bg-warning">
 					<div class="brand">
-						<img class="w-100" src="./views/images/np.png" alt="logo">
+						<img class="w-100" src="./views/images/avt.png" alt="logo">
 					</div>
 					<div class="card-body mb-3 mt-3">
 						<div class="row col-md-12 text-light ms-0">
