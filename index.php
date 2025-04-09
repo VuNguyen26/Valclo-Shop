479e2322951f42e689adafb22ed80fc5a8bccc96
subway90 <nguyenhieu3105@gmail.com>
2025-04-10 00:55:01 +0700

    add func test & test_array

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 session_start();
+/**
+ * Dùng để trả về các thông số của $array
+ * @param $array Mảng cần hiển thị
+ * @return array
+ */
+function test_array($array) {
+    echo'<pre>';
+    print_r($array) ;
+    echo'</pre>';
+    exit;
+}
+
+/**
+ * Dùng để trả về các thông số của $input
+ * @param $input Giá trị cần hiển thị
+ * @return mixed
+ */
+function test($input) {
+    var_dump($input);
+    exit;
+}
+
 // Process URL from browser
 require_once "./Function/App.php";
 
